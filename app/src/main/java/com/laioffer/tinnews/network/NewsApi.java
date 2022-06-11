937d130e13a707592feebd6effd829769cdecc89
jxnwang <89869339+jxnwang@users.noreply.github.com>
2022-06-11 12:27:19 -0500

    2

--- app/src/main/java/com/laioffer/tinnews/network/NewsApi.java
+++ app/src/main/java/com/laioffer/tinnews/network/NewsApi.java
@@ -1,4 +1,20 @@
 package com.laioffer.tinnews.network;
 
-public class NewsApi {
+import com.laioffer.tinnews.model.NewsResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NewsApi {
+
+    //retrofit will generate a class that implements this.
+
+    @GET("top-headlines")
+    Call<NewsResponse> getTopHeadlines(@Query("country") String country);
+
+    @GET("everything")
+    Call<NewsResponse> getEverything(
+            @Query("q") String query, @Query("pageSize") int pageSize);
+
 }
