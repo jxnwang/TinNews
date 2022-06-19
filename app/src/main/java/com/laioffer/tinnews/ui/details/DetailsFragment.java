4ca03297f303da7dffc3442fa51016f095955ffc
jxnwang <89869339+jxnwang@users.noreply.github.com>
2022-06-19 17:21:53 -0500

    final

--- app/src/main/java/com/laioffer/tinnews/ui/details/DetailsFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/details/DetailsFragment.java
@@ -20,41 +20,20 @@ public class DetailsFragment extends Fragment {
 
     private FragmentDetailsBinding binding;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public DetailsFragment() {
         // Required empty public constructor
-    }
-
-
-    public static DetailsFragment newInstance(String param1, String param2) {
-        DetailsFragment fragment = new DetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details2, container, false);
+        binding = FragmentDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
